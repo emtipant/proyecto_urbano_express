ab11a452c9c84bc4f7fd79782b03e6dcca7bcc07
ERIKA <em.tipant@uea.edu.ec>
2026-02-21 20:49:13 -0500

    Proyecto Urbano Express

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Ruta principal
+@app.route('/')
+def inicio():
+    return "Bienvenido a Urbano Express - Sistema de Delivery Urbano"
+
+# Ruta dinámica pedido
+@app.route('/pedido/<cliente>')
+def pedido(cliente):
+    return f"Hola {cliente}, tu pedido está en proceso de envío."
+
+# Ruta dinámica seguimiento
+@app.route('/seguimiento/<codigo>')
+def seguimiento(codigo):
+    return f"El pedido con código {codigo} está en camino."
+
+if __name__ == '__main__':
+    app.run(debug=True)
